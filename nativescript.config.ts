29aa2d5f048453f52ca5ff10a151dc4d0ee3c1bc
badoge <18620902+badoge@users.noreply.github.com>
2025-07-04 20:46:25 +0400

    :)

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,12 +1,12 @@
-import { NativeScriptConfig } from '@nativescript/core';
+import { NativeScriptConfig } from "@nativescript/core";
 
 export default {
-  id: 'com.alienpls.nothing',
-  appPath: 'app',
-  appResourcesPath: 'App_Resources',
-  version: '1.0.0', 
+  id: "com.alienpls.nothing",
+  appPath: "app",
+  appResourcesPath: "App_Resources",
+  version: "1.0.1",
   android: {
-    v8Flags: '--expose_gc',
-    markingMode: 'none'
-  }
+    v8Flags: "--expose_gc",
+    markingMode: "none",
+  },
 } as NativeScriptConfig;
